83d67fd05adc746e4d62c76038601161433dcb58
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-06-29 01:34:45 -0400

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+	appName: 'platzimusic',
+	apiKey: 'd9d14de5769002a3710e9a99eb69207f',
+	secret: '3e674718fea002d66150db4b593c6383',
+	registeredTo: 'wgiovanni95'
+}
+
+//Application name	platzimusic
+//API key	d9d14de5769002a3710e9a99eb69207f
+//Shared secret	3e674718fea002d66150db4b593c6383
+//Registered to	wgiovanni95
