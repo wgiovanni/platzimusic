b2ef262bb1c5e9b81dafb6ac395fb75ff20c9805
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-06-29 01:47:25 -0400

    Correción al cargar la imagen logo.png de PlatziMusic al hacer deploy a Github Pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='https://wgiovanni.github.io/platzimusic/dist/logo.png')
+    img(src='dist/logo.png')
     h1 PlatziMusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" :value="country.value") {{ country.name }}
